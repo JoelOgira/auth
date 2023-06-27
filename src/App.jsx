4c8a58d048db6b19b811957c047bfd8c1cc0989d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-27 17:33:49 +0300

    Pulled in login component for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import ClockApp from "./timer/ClockApp";
 import Register from "./Auth/Register";
+import Login from "./Auth/Login";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/clock" element={<ClockApp />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
